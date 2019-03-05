3a98ac32a8d76485dd92253fd4615fd3882eb1c6
wangGHS <48207160+wangGHS@users.noreply.github.com>
2019-03-05 08:11:17 +0800

    aaa
    aaa

--- src/Git/GitTest.java
+++ src/Git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("aaabb");
 		System.out.println("kkbb");
-		System.out.println("aacc");
+		System.out.println("xxxx");
 	}
 }
